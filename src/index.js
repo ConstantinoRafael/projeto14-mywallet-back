6e5b7627c3c052baa2e69bb0ec711756d1efe61a
ConstantinoRafael <rafael.constantino89@gmail.com>
2022-11-15 04:45:39 -0300

    feat: connection to MongoDB

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
 import express from "express";
+import dotenv from "dotenv";
+import { MongoClient } from "mongodb";
 
 const app = express();
+dotenv.config();
+app.use(express.json());
+
+const mongoClient = new MongoClient(process.env.MONGO_URI);
+
+try {
+  await mongoClient.connect();
+  console.log("MongoDB connected");
+} catch (err) {
+  console.log(err);
+}
+
+const db = mongoClient.db("mywallet");
 
 
-app.listen(5000, () => console.log("server running in port: 5000"))
+app.listen(process.env.PORT, () =>
+  console.log(`Server running in port: ${process.env.PORT}`)
+);
