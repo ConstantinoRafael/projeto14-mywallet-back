b6a29471875fe84070117516067dd615d118a02f
ConstantinoRafael <rafael.constantino89@gmail.com>
2022-11-21 14:51:34 -0300

    feat: add variable port

--- src/index.js
+++ src/index.js
@@ -20,4 +20,6 @@ export const userSchema = joi.object({
   confirmPassword: joi.ref("password"),
 });
 
-app.listen(5000, () => console.log(`Server running in port: 5000`));
+const port = process.env.PORT || 5000
+
+app.listen(port, () => console.log(`Server running in port: ${port}`));
